2d014f0733a08f6d5ddde862eb2f364e538d8125
KAUÃ GUILHERME ZASTROW <kaua_zastrow@estudante.sesisenai.org.br>
2026-06-02 21:54:28 -0300

    lig4

--- testes/teste.js
+++ testes/teste.js
@@ -1,71 +1,26 @@
-// const num1 = 121
-// const num2 = 123
-// const num3 = 11211
-// let num = []
-// num.push(num1, num2, num3)
-// for (let i = 0; i < num.length; i++) {
-//     if ((num[i].toString().split("").reverse().join()) === (num[i].toString().split("").join())) { console.log("true", num[i]) } else { console.log("false", num[i]) }
-// }
-
-
-
-
-
-let lerTeclado = require("readline-sync")
-console.log("Este é um jogo da velha classico.\nPara jogar escolha qual linha desejada e qual coluna desejada.\nO primeiro jogador sera o [X] e o segundudo o [O].")
-let vitoriasX = 0
-let vitoriasO = 0
-while (true) {
-    let i = 0
-    let vitoria = false
-    let jogoVelha = [
-        ["-", "-", "-"],
-        ["-", "-", "-"],
-        ["-", "-", "-"]
-    ]
-    while (true) {
-        console.table(jogoVelha)
-        console.log(i)
-        let a = i % 2 === 0 ? "X" : "O"
-        console.log(`Jogador ${a}`)
-        let linha = lerTeclado.questionInt("linha: ") - 1
-        let coluna = lerTeclado.questionInt("coluna: ") - 1
-        if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2) {
-            console.log("Linha ou coluna não encontrados. Tente novamente.")
-            continue
-        } else if (jogoVelha[linha][coluna] == "X" || jogoVelha[linha][coluna] == "O") {
-            console.log("Ja esta em uso. Tente novamente.")
-            continue
-        }
-        jogoVelha[linha][coluna] = a
-        i++
-        console.table(jogoVelha)
-        for (let k = 0; k < jogoVelha[0]; k++) {
-            if ((jogoVelha[k][0] == a && jogoVelha[k][1] == a && jogoVelha[k][2] == a) || (jogoVelha[0][k] == a && jogoVelha[1][k] == a && jogoVelha[2][k] == a)) {
-                vitoria = true
-            }
-        }
-        if (jogoVelha[0][0] == a && jogoVelha[1][1] == a && jogoVelha[2][2] == a) { vitoria = true }
-        if (jogoVelha[2][2] == a && jogoVelha[1][1] == a && jogoVelha[0][0] == a) { vitoria = true }
-        // if (jogoVelha[0][0] === a && jogoVelha[0][1] == a && jogoVelha[0][2] == a) { vitoria = true }
-        // if (jogoVelha[1][0] === a && jogoVelha[1][1] == a && jogoVelha[1][2] == a) { vitoria = true }
-        // if (jogoVelha[2][0] === a && jogoVelha[2][1] == a && jogoVelha[2][2] == a) { vitoria = true }
-        // //////////
-        // if (jogoVelha[0][0] == a && jogoVelha[1][0] == a && jogoVelha[2][0] == a) { vitoria = true }
-        // if (jogoVelha[0][1] == a && jogoVelha[1][1] == a && jogoVelha[2][2] == a) { vitoria = true }
-        // if (jogoVelha[0][2] == a && jogoVelha[1][2] == a && jogoVelha[2][2] == a) { vitoria = true }
-        //////////
-        // if (jogoVelha[0][0] == a && jogoVelha[1][1] == a && jogoVelha[2][2] == a) { vitoria = true }
-        // if (jogoVelha[2][2] == a && jogoVelha[1][1] == a && jogoVelha[0][0] == a) { vitoria = true }
-        if (vitoria === true) {
-            if (jogoVelha[linha][coluna] === "X") { vitoriasX++ } else { vitoriasO++ }
-            console.log(`${jogoVelha[linha][coluna]} Venceu! Com ${jogoVelha[linha][coluna] === "X" ? vitoriasX : vitoriasO} vitorias!`)
-            break;
-        } else if (i === 9) {
-            console.log(`Velha!\nTotal de pontos:\nX = ${vitoriasX}.\nO = ${vitoriasO}.`)
-            break;
+const lerTeclado = require("readline-sync")
+console.log("LIG-4")
+let lig4 = [
+    ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]"],
+    ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]"],
+    ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]"],
+    ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]"],
+    ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]"],
+    ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]"]
+]
+// ⭕🧊
+//pA = peça atual
+let jogadas = 0
+console.log("⭕🧊")
+function vencedor() {
+    let vitoria1 = false //⭕
+    let vitoria2 = false //🧊
+    for (let l = lig4.length - 1; l >= 0; l--) {
+        for (let c = 0; c < lig4[0].length; c++) {
+            if (lig4[l][c] != "[  ]") pA = lig4[l][c]
+            if (lig4[l][c] === pA && lig4[l][c + 1] === pA && lig4[l][c + 2] === pA && lig4[l][c + 3] === pA || lig4[l][c] === pA && lig4[l + 1][c] === pA && lig4[l + 2][c] === pA && lig4[l + 3][c] === pA) pA === "🔴" ? vitoria1 = true : vitoria2 = true
+            if (lig4[l][c] === pA && lig4[l + 1][c + 1] === pA && lig4[l + 2][c + 2] === pA && lig4[l + 3][c + 3] === pA || lig4[l][c] === pA && lig4[l - 1][c - 1] === pA && lig4[l - 2][c - 2] === pA && lig4[l - 3][c - 3] === pA) pA === "🔴" ? vitoria1 = true : vitoria2 = true
         }
     }
-    continuarJogoAmigo = !lerTeclado.keyInYN("Deseja continuar jogando?")
-    if (continuarJogoAmigo) { continue }
+    if (jogadas === 41){}
 }
