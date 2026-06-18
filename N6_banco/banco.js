31829dcfb856088a526317cfd97d61afe0bd1d19
KAUÃ GUILHERME ZASTROW <kaua_zastrow@estudante.sesisenai.org.br>
2026-06-17 21:41:22 -0300

    banco

---
+++ N6_banco/banco.js
@@ -0,0 +1,55 @@
+const lT = require("readline-sync")
+// Banco
+/*
+
+*/
+const taxa = {
+    // ==============taxa de deposito==============|
+    deposito_7a12: 0.15,
+    deposito_12a18: 0.10,
+    deposito_18a7: 0.25,
+    // ==============taxa de saque=================|
+    saque_7a12: 0.10,
+    saque_12a18: 0.08,
+    saque_18a7: 0.20,
+}
+function usuarioBase() {
+    usuario = {
+        info_pessoais: {
+            nome: "John",
+            Idade: 19,
+            cpf: "45878935499",
+            telefone: "(55) 15 996765994",
+            email: "johnLock@gmail.com",
+            rendaFixa: 1800.00
+        },
+        saldo: {
+            contaCorrente: 0,
+            poupanca: 0,
+            investimento: 0
+        },
+        historico: [
+            {
+                tipo: "Deposito",
+                valor: 1200.00,
+                data: "12/21/2012",
+                hora: "21:43"
+            }
+        ],
+        tipo: "Cartão ",
+        logado: false,
+        limites: {},
+        cep: "789456541",
+    }
+    usuario.limites = {
+        //-------------------DEPOSITO-----------------------
+        limiteDeposito_7a12: 2 * usuario.info_pessoais.rendaFixa,
+        limiteDeposito_12a18: 3 * usuario.info_pessoais.rendaFixa,
+        limiteDeposito_18a7: usuario.info_pessoais.rendaFixa,
+        //--------------------SAQUE-------------------------
+        limiteSaque_7a12: 1.5 * usuario.info_pessoais.rendaFixa,
+        limiteSaque_12a18: 2 * usuario.info_pessoais.rendaFixa,
+        limiteSaque_18a7: 0.5 * usuario.info_pessoais.rendaFixa
+    }
+    return usuario
+}
